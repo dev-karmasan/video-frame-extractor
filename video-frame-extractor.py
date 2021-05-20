51f87e6e667f5e3fc5131399d706cf1552da42c0
Sandeep Karmacharya <sand.deep.pro@gmail.com>
2021-05-20 22:21:03 +0200

    video-frame-extractor_v1
    Fixed the GUI issue

--- video-frame-extractor.py
+++ video-frame-extractor.py
@@ -25,7 +25,7 @@ image_output_selection.set(images_options[0]) # default selection for image outp
 digit_number_selection = IntVar() # integer variable for digit number selection
 digit_number_selection.set(digit_options[1]) # default selection for digit number
 
-# functions for section 1: Loading files section
+## functions for section 1: Loading files section
 def loadFiles():
     global filenamespath_list
 
@@ -36,26 +36,24 @@ def loadFiles():
     filenamespath_list = list(root.filenames) # List to store filepaths
 
     filenames_list = [] # List to store filenames
-           
-    # Extract filenames from filepaths
-    # filenames_list += [Path(i).stem for i in filenamespath_list]
 
     # Makes each line for the paths
     filenamespath = '\n'.join([i for i in filenamespath_list])
     
     # update file list label
-    file_list = Label(loading_frame, text = filenamespath, font = ('Consolas', 16))
+    file_list.insert(END, filenamespath) # insert texts on text box
+    file_list.configure(state = 'disabled') # create text read only
     file_list.grid(row = 0, column = 1)
 
 
 def clear():
-    file_list = Label(loading_frame, text = '', padx = 300, pady = 60, bd = 1)
-    file_list.grid(row = 0, column = 1)
+    file_list.configure(state = 'normal')
+    file_list.delete("1.0", "end")
     filenamespath_list = [] # List to store filepaths
     filenames_list = [] # List to store filenames
 
 
-# functions for section 3: Start the extraction
+## functions for section 3: Start the extraction
 def execution(fps, image_output_selection, digit_number_selection, filenamespath_list):
     path = os.getcwd() # Get current working directory
     ffmpeg_plugin_path = path + "\\bin\\ffmpeg.exe"
@@ -67,6 +65,9 @@ def execution(fps, image_output_selection, digit_number_selection, filenamespath
         if os.path.isdir(filename_with_path) == False:
             os.mkdir(filename_with_path)
         subprocess.run([ffmpeg_plugin_path, '-i', filenamespath, '-r', str(fps), filename_with_path + '/' + filename + '_%0' + str(digit_number_selection) + 'd.' + image_output_selection])
+
+    subprocess.terminate()
+    messagebox.info('Process completed')
 
 def start():
     if image_output_selection.get() == 'jpg':
@@ -89,14 +90,19 @@ def start():
 
 
 
-#-------------------------------------------------------------------------------
+##-------------------------------------------------------------------------------
 # Section 1: Loading files section
 loading_frame = LabelFrame(root, text = 'load your files')
 loading_frame.pack(padx = 5, pady = 5)
 
-# label to show the selected videos
-file_list = Label(loading_frame, text = '', padx = 300, pady = 60, bd = 1)
+# Textbox to show the selected videos
+file_list = Text(loading_frame, padx = 15, pady = 15, bd = 1, width = 50, height = 8)
 file_list.grid(row = 0, column = 1)
+
+# Adding scroll bar to the text box
+scroll_bar = Scrollbar(loading_frame, orient = 'vertical', command = file_list.yview)
+file_list.config(yscrollcommand = scroll_bar.set)
+file_list.grid(row = 0, column = 1, sticky = NSEW)
 
 # button for loading videos
 button_load = Button(loading_frame, text = 'Load', padx = 40, pady = 20, command = loadFiles)
@@ -106,11 +112,10 @@ button_load.grid(row = 1, column = 0, padx = 5, pady = 5)
 button_clear = Button(loading_frame, text = 'Clear', padx = 40, pady = 20, command = clear)
 button_clear.grid(row = 1, column = 2, padx = 5, pady = 5)
 
-#-----------------------------------------------------------
+##-----------------------------------------------------------
 # Section 2: Extraction options
-options_frame = LabelFrame(root, text = 'select your options', padx = 200, pady = 20)
+options_frame = LabelFrame(root, text = 'select your options', padx = 80, pady = 20)
 options_frame.pack(padx = 5, pady = 5)
-
 
 # Number of frames per second
 Label(options_frame, text = 'Frames per second', padx = 40, pady = 30, bd = 1).grid(row = 0, column = 0)
@@ -128,11 +133,9 @@ digit_number = OptionMenu(options_frame, digit_number_selection, *digit_options)
 digit_number.grid(row = 1, column = 2)
 
 
-
-
-#-----------------------------------------------------------------------------------------
+##-----------------------------------------------------------------------------------------
 # Section 3: Start the extraction
-execution_frame = LabelFrame(root, text = 'Start the extraction', padx = 300, pady = 20)
+execution_frame = LabelFrame(root, text = 'Start the extraction', padx = 50, pady = 20)
 execution_frame.pack(padx = 5, pady = 5)
 
 button_start = Button(execution_frame, text = 'Start', padx = 40, pady = 20, command = start)
